61a1365d4dbf8d82e01e7b5e1c3427ae65ce7713
CristianoSellit <99354780+CristianoSellitto@users.noreply.github.com>
2022-04-28 17:09:33 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,14 @@
+// Cristiano
+// ICS2O-Unit5-03-CSharp
+// April 28 2022
+
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World");
+        Console.WriteLine("\nFinished.");
+    }
+}
