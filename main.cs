307bfac2ea744da27067ee0315ead0e6a74ce926
CristianoSellit <99354780+CristianoSellitto@users.noreply.github.com>
2022-04-28 17:18:43 +0000

    Added program

--- main.cs
+++ main.cs
@@ -8,7 +8,36 @@ class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello World");
+        string ageGroup;
+
+        void seeEligibleMovieTypes()
+        {
+            Console.WriteLine("What’s your age group?");
+            Console.WriteLine("1 - 17 or over");
+            Console.WriteLine("2 - 13 to 16");
+            Console.WriteLine("3 - 12 and under");
+            ageGroup = Console.ReadLine();
+            if (ageGroup == "1")
+            {
+                Console.WriteLine("\nYou can watch R, PG-13, and G rated movies alone.");
+            }
+            else if (ageGroup == "2")
+            {
+                Console.WriteLine("\nYou can watch PG-13 and G rated movies alone.");
+            }
+            else if (ageGroup == "3")
+            {
+                Console.WriteLine("\nYou can watch a G rated movies alone.");
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Please select a valid response.");
+                seeEligibleMovieTypes();
+            }
+        }
+
+        seeEligibleMovieTypes();
         Console.WriteLine("\nFinished.");
     }
 }
